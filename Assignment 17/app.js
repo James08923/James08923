12a30a37f51ba85fba0312297880a283d471b0f8
Ahmed ali khan <159822322+James08923@users.noreply.github.com>
2024-02-26 13:00:22 +0500

    assignment 17

---
+++ Assignment 17/app.js
@@ -0,0 +1,77 @@
+alert("HELLO WORLD!");
+
+
+
+//CHAPTER NO 4
+//Q1
+// document.write("$-1200");
+ 
+
+//Q2
+// document.write("hello");
+
+
+//Q3
+// document.write("world");
+
+
+//q4
+// document.write("$-my");
+
+
+//Q5
+// document.write("ok");
+
+
+
+
+
+
+//Q1
+// document.write(@ ahmed);
+
+
+//Q2
+// document.write(#00012);
+
+
+//Q3
+// document.write(75%);
+
+
+//Q4
+// document.write(&and);
+
+
+//Q5
+// document.write(@ali);
+
+
+
+//CHAPTER NO 5
+//Q1
+// document.write("");
+
+
+//Q2
+//document.write("Value after variable declaration is undefined<br/>Initial value:5");
+
+
+
+
+
+
+
+var num = 4;
+
+document.write(num+"*1="+num*1+"<br/>"+
+num+"*2="+num*2+"<br>"+
+num+"*3="+num*3+"<br>"+
+num+"*4="+num*4+"<br>"+
+num+"*5="+num*5+"<br>"+
+num+"*6="+num*6+"<br>"+
+num+"*7="+num*7+"<br>"+
+num+"*8="+num*8+"<br>"+
+num+"*9="+num*9+"<br>"+
+num+"*10="+num*10+"<br>"
+)
