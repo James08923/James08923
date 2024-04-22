4292654d624678e12d96d675a045c4ccdcefc181
Ahmed ali khan <159822322+James08923@users.noreply.github.com>
2024-04-22 21:19:34 +0500

    class work

---
+++ app.js
@@ -0,0 +1,59 @@
+// function getValue(val) {
+//     var input = document.getElementById("input");
+//     input.value += val;
+// }
+
+// function clrAll() {
+//     var input = document.getElementById("inputField");
+//     input.value = "";
+// }
+
+// function squareNum() {
+//     var input = document.getElementById("inputField");
+//     input.value = input.value * input.value;
+// }
+
+// function deletChar() {
+//     var input = document.getElementById("inputField");
+//     input.value = input.value.slice(0, -1);
+// }
+
+// function equal() {
+//     var input = document.getElementById("inputField");
+//     input.value = eval(input.value);
+// }
+
+
+
+function submit () {
+    var name = document.getElementById("username");
+    var email = document.getElementById("password");
+}
+if (username.value && password.value) {
+    console.log(username.value);
+    console.log(password.value);
+}
+else {
+    alert("Please fill all the fields");
+}
+
+
+// function expandPara() {
+    // var shortText = "Lorem ipsum dolor sit amet";
+    // var CompletText =
+    // "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aperiam totam quia, distinctio, pariatur hic eveniet deleniti et omnis, necessitatibus esse provident fugiat laborum asperiores animi corporis iste autem obcaecati repellat. Ab, amet tempora quibusdam unde sint, dolore in recusandae illo laborum laboriosam sunt quae consectetur. Laboriosam, officiis?";
+    // var para = document.getElementById("para");
+    // var link = document.getElementById("link");
+
+    // if (link.innerHTML === "read more") {
+    //     para.innerHTML = CompletText;
+    //     link.innerHTML = "read less";
+    // } else {
+    //     para.innerHTML = shortText;
+    //     link.innerHTML = "read more";
+    // }
+}
+
+
+
+
